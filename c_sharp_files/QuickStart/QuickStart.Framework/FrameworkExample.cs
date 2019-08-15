3398e14d756237aaf098c146e734b1944d86974f
Kenny Urena <urenakenny@gmail.com>
2019-08-15 18:58:04 -0400

    Added two C# projects

---
+++ c_sharp_files/QuickStart/QuickStart.Framework/FrameworkExample.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickStart.Framework
+{
+  public class FrameworkExample
+  {
+    public async Task<object> GetAppDomainDirectory(dynamic input)
+    {
+      return AppDomain.CurrentDomain.BaseDirectory;
+    }
+
+    public async Task<object> GetCurrentTime(dynamic input)
+    {
+      return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+    }
+
+    public async Task<object> UseDynamicInput(dynamic input)
+    {
+      return $".NET Core welcomes {input}";
+    }
+  }
+}
